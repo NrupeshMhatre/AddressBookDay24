b6a292c76b3df971687162479fd8048f67c8c335
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-21 17:38:18 +0530

    No Duplicate Entry For Same Person

--- src/main/java/practice/AddressBook.java
+++ src/main/java/practice/AddressBook.java
@@ -1,7 +1,17 @@
 package practice;
 
+import java.util.List;
+
 public class AddressBook {
-public static void main(String[] args) {
-	System.out.print("AddressBook Stream ");
+	private List<Contact> contacts;
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
 }
-}
+
